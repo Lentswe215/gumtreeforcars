9508b90a8918fa7ee50a1bab08788360a96d221d
Lentswe215 <ephraimmamonong@gmail.com>
2020-05-28 19:02:35 +0200

    made changes

--- carsforsale/views.py
+++ carsforsale/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def cars(request):
-    return render(request, "carsforsale/cars.html")
+    return render(request, "carsforsale/cars.html",)
     
